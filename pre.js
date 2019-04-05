2555b22b9d2ea348dd09c370fc0fc7ce26b5c449
Rafael Gieschke <rafael.gieschke@rz.uni-freiburg.de>
2019-04-05 16:38:01 +0200

    Fix: picotcp.wasm cannot be loaded on Node.js/Windows

--- pre.js
+++ pre.js
@@ -1,6 +1,14 @@
 Module["locateFile"] = function(path) {
   let url = import.meta.url;
   url = url.replace(/^file:\/\//, "");
+  // HACK: Special case for Node.js on Windows
+  // (`url` will look like "file:///C:/...").
+  // Would properly use `require("url").fileURLToPath(url)`
+  // on all Node.js platforms, which is not avaible
+  // on older Node.js versions, though.
+  try {
+    if (process.platform === "win32") url = url.replace(/^\/+/, "");
+  } catch {}
   return url + "/../" + path;
 };
 Module["noExitRuntime"] = true;
