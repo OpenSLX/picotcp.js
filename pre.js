a68205e06ec8686102e3e7cf61da3419b972103b
Rafael Gieschke <rafael.gieschke@rz.uni-freiburg.de>
2019-04-25 17:39:34 +0200

    HACK: Fix Emscripten's meddling with uncaughtException/unhandledRejection
    See https://github.com/emscripten-core/emscripten/issues/7855

--- pre.js
+++ pre.js
@@ -12,3 +12,24 @@ Module["locateFile"] = function(path) {
   return url + "/../" + path;
 };
 Module["noExitRuntime"] = true;
+
+// HACK: Work around https://github.com/emscripten-core/emscripten/issues/7855
+// for Node.js: turn process.on("uncaughtException" | "unhandledRejection", ...)
+// into no-op.
+let process;
+try {
+  process = new Proxy(global.process, {
+    get(target, key, receiver) {
+      const ret = Reflect.get(target, key, receiver);
+      if (key !== "on") return ret;
+      return new Proxy(ret, {
+        apply(target, thisArg, args) {
+          if (args[0] !== "uncaughtException"
+            && args[0] !== "unhandledRejection") {
+            return Reflect.apply(target, thisArg, args);
+          }
+        }
+      });
+    }
+  });
+} catch {}
